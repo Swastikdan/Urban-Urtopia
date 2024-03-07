3ceba86c171584f86aa89f0b1b30a7ac03388558
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-03-08 02:19:52 +0530

    backend 👍

--- app/page.jsx
+++ app/page.jsx
@@ -1,74 +1,7 @@
-"use client";
-import React, { useCallback } from "react";
-import { useSession } from "next-auth/react";
+import React from 'react'
 
-export default function Page() {
-  const { data: session } = useSession();
-  const userid = session?.user.id;
-  const sendData = useCallback(() => {
-    let data = {
-      title: "Manali will be Back Soon!",
-      address: "Manali, Himachal Pradesh, India",
-      state: "Himachal Pradesh",
-      city: "Manali",
-      street: "",
-      photos: [
-        "https://res.cloudinary.com/dp5tomvwb/image/upload/v1709405242/Airbnb/Places/sk8xwpzqprsspc0d5h7s.jpg",
-        "https://res.cloudinary.com/dp5tomvwb/image/upload/v1709405250/Airbnb/Places/ejbtqgvsvqtfhcsrceed.jpg",
-        "https://res.cloudinary.com/dp5tomvwb/image/upload/v1709405251/Airbnb/Places/dqzsf9v6klbxmeqzagkj.jpg",
-        "https://res.cloudinary.com/dp5tomvwb/image/upload/v1709405252/Airbnb/Places/zjqdj8e3zp2to0otz9xg.jpg",
-        "https://res.cloudinary.com/dp5tomvwb/image/upload/v1709405253/Airbnb/Places/u0oraoifctcxwgy1tyzy.jpg",
-      ],
-      description:
-        "Ménage - By The Beas , A colonial style hill cottage near Manali, this delightful vacation home promises the perfect mix of hills with a scenic river side in the privacy of your own space. It offers a great stay on the river bank amidst a small Apple orchard. Tastefully done up interiors, 3 cosy bedrooms, a large living room with fireplace, sun bathing attic with a view of the river and mountains, bon-fire & barbecue by our cook, absolutely perfect for a laid-back friends or family staycation.",
-      category: [
-        "amazing-views",
-        "rooms",
-        "tropical",
-        "farms",
-        "tiny-homes",
-        "historical-homes",
-        "trending",
-        "camping",
-        "cabins",
-      ],
-      maxGuests: 9,
-      price: 9000,
-      petsAllowed: false,
-      extraInfo: "Some extra Info",
-      status: "approved",
-    };
-
-   data.ownerId = "65e8d4d9d5f4c3596efc00e4"; // Check the value of data after setting ownerId
-
-    const routeUrl =
-      "http://localhost:3000/api/places/edit/65ea13285555c6ecd9ac9d74";
-
-    fetch(routeUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }, []);
-
+export default function page() {
   return (
-    <div>
-      {session && <h1>Session {session.user.id}</h1>}
-      <button onClick={sendData}>Send Data</button>
-    </div>
-  );
+    <div>page</div>
+  )
 }
