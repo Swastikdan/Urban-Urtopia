60d13bad76df35153bd88fedc51151b8c64e62a2
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-03-07 20:15:27 +0530

    multiple file works

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -3,19 +3,24 @@ const cloudinary = require('cloudinary').v2;
 import { join } from "path";
 import { writeFile } from "fs/promises";
 export async function upload(request) {
-  const data = await request.formData();
-  const file = data.get("file");
-  if (!file)
-    return NextResponse.json({ message: "No file found" }, { status: 400 });
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
-    const path = join('/','/tmp', file.name);
-    await writeFile(path, buffer);
-    let result = await cloudinary.uploader.upload(path, {
-      folder: 'Airbnb/Places',
-    });
-    return NextResponse.json(result.secure_url);
+    const data = await request.formData();
+    const files = data.getAll("photos");
+    if (!files || files.length === 0)
+        return NextResponse.json({ message: "No files found" }, { status: 400 });
 
+    let urls = [];
+    for (let file of files) {
+        const bytes = await file.arrayBuffer();
+        const buffer = Buffer.from(bytes);
+        const path = join('/','/tmp', file.name);
+        await writeFile(path, buffer);
+        let result = await cloudinary.uploader.upload(path, {
+            folder: 'Airbnb/Places',
+        });
+        urls.push(result.secure_url);
+    }
+
+    return NextResponse.json(urls);
 }
 
 export { upload as POST };
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,16 @@
-"use client"
+"use client";
 import React, {useState, useEffect} from 'react'
 
 export default function page() {
 
-  const [file, setFile] = useState(null)
+  const [files, setFiles] = useState([])
 
   const onSubmit = async (e) => {
     e.preventDefault()
     const formData = new FormData()
-    formData.append('file', file)
+    files.forEach((file, index) => {
+      formData.append('photos', file)
+    })
     const res = await fetch('/api/upload', {
       method: 'POST',
       body: formData
@@ -16,22 +18,15 @@ export default function page() {
     const data = await res.json()
     console.log(data)
   }
+
   return (
    <>
-   
    <div>
-
-    <form onSubmit={onSubmit} >
-
-
-
-    <input type="file" name="file" id="file" onChange={(e)=>setFile(e.target.files?.[0])}  />
-
-    <input type="submit" value="Upload" />  
+    <form onSubmit={onSubmit}>
+      <input type="file" name="photos" id="photos" multiple onChange={(e) => setFiles([...e.target.files])} />
+      <input type="submit" value="Upload" />  
     </form>
    </div>
-   
-   
    </>
   )
 }
