565ed3534490a44cf6a50cfac84547bc0d3d6d88
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-03-23 12:41:14 +0530

    suspense removed form place

--- app/place/[id]/page.jsx
+++ app/place/[id]/page.jsx
@@ -12,14 +12,14 @@ export default async function page({ params }) {
   return (
     <div className="mx-auto flex w-full max-w-screen-xl flex-col  items-center">
       <div className="flex w-full sm:hidden">
-        <Suspense fallback={<div>Loading...</div>}>
+      
           <PlacePageMobile place={place} />
-        </Suspense>
+       
       </div>
       <div className="hidden px-4  sm:flex xl:px-16">
-        <Suspense fallback={<div>Loading...</div>}>
+        
           <PlacePageDesktop place={place} />
-        </Suspense>
+ 
       </div>
     </div>
   );
