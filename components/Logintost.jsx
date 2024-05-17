e3e48b4364b2f85917c2006e6886ca22122b454f
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-05-17 22:50:23 +0530

    .

--- components/Logintost.jsx
+++ components/Logintost.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect } from 'react'
 import { toast } from 'sonner';
-import { useRouter } from 'next/navigation'; // Corrected from 'next/navigation'
+import { useRouter } from 'next/navigation'; 
 
 export default function Logintost({ logintype, login }) {
     const router = useRouter();
