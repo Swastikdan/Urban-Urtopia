c89909e463261d310798d25b2c3500d619fd3137
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-05-27 01:36:43 +0530

    desktop search done
    Need to optimize

--- components/searchbar/SearchBar.jsx
+++ components/searchbar/SearchBar.jsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
+import { getCityNames } from '@/utils/cities';
 import {
   Drawer,
   DrawerClose,
@@ -20,6 +21,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Calendar } from '@/components/ui/calendar';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { formatRangeDate } from '@/utils';
 import {
   Search,
@@ -29,8 +31,12 @@ import {
   Minus,
   Share,
   Heart,
+  MapPin,
+  X,
 } from 'lucide-react';
 import { addDays, isBefore, startOfDay } from 'date-fns';
+// import { title } from 'process';
+// import { filter } from 'indian-cities-database/cities';
 export default function SearchBar() {
   const searchParams = useSearchParams();
   const router = useRouter();
@@ -53,77 +59,6 @@ export default function SearchBar() {
     infants: 0,
     pets: 0,
   });
-
-  // OLD CODE
-  // const [date, setDate] = useState({
-  //   from: null,
-  //   to: null,
-  // });
-  // const setAdults = (value) => {
-  //   if (value + searchState.children <= 16) {
-  //     setSearchState((prevState) => ({ ...prevState, adults: value }));
-  //   }
-  // };
-
-  // const setChildren = (value) => {
-  //   if (value + searchState.adults <= 16) {
-  //     setSearchState((prevState) => ({ ...prevState, children: value }));
-  //   }
-  // };
-
-  // const setInfants = (value) => {
-  //   setSearchState((prevState) => ({ ...prevState, infants: value }));
-  // };
-
-  // const setPets = (value) => {
-  //   setSearchState((prevState) => ({ ...prevState, pets: value }));
-  // };
-
-  // useEffect(() => {
-  //   let newState = {};
-  //   params.forEach((param) => {
-  //     let value = searchParams.get(param);
-  //     if (value) {
-  //       newState[param] = value;
-  //     }
-  //   });
-  //   setSearchState((prevState) => ({ ...prevState, ...newState }));
-  // }, [searchParams]);
-
-  // add a useeffect if the childen has valur and adults is empty then set the adults to 1
-
-  // useEffect(() => {
-  //   if (Number(searchState.children) > 0 && Number(searchState.adults) === 0) {
-  //     setSearchState((prevState) => ({ ...prevState, adults: '1' }));
-  //   }
-  // }, [searchState.children, searchState.adults]);
-
-  // if the children is greater than 0 and adults is 0, add 1 to adults
-  //   useEffect(() => {
-  //     if (Number(searchState.children) > 0 && Number(searchState.adults) === 0) {
-  //       setSearchState((prevState) => ({
-  //         ...prevState,
-  //         adults: Number(searchState.adults) + 1,
-  //       }));
-  //     }
-  //   }, [searchState.children, searchState.adults]);
-
-  // const handleSearch = () => {
-  //   if (!searchState.adults) {
-  //     return;
-  //   }
-
-  //   let search = '';
-  //   params.forEach((param) => {
-  //     if (searchState[param]) {
-  //       search += `${param}=${searchState[param]}&`;
-  //     }
-  //   });
-  //   router.push(`/?${search}`);
-  // };
-
-  // OLD CODE END
-
   const setAdults = (value) => {
     setSearchState((prevState) => ({ ...prevState, adults: value }));
   };
@@ -220,7 +155,57 @@ export default function SearchBar() {
     return () => clearTimeout(timer);
   }, []);
 
-  console.log(searchState);
+  const topcities = [
+    {
+      title: "I'm Flexiable",
+      img: 'https://res.cloudinary.com/debewnh29/image/upload/v1716749383/nestly/public/pictures/cities/anywhere.jpg',
+      value: '',
+    },
+    {
+      title: 'Mumbai',
+      img: 'https://res.cloudinary.com/debewnh29/image/upload/nestly/public/pictures/cities/mumbai.avif',
+      value: 'mumbai',
+    },
+    {
+      title: 'Delhi',
+      img: 'https://res.cloudinary.com/debewnh29/image/upload/nestly/public/pictures/cities/delhi.avif',
+      value: 'delhi',
+    },
+    {
+      title: 'Bangalore',
+      img: 'https://res.cloudinary.com/debewnh29/image/upload/nestly/public/pictures/cities/bengaluru.webp',
+      value: 'bangalore',
+    },
+
+    {
+      title: 'Chennai',
+      img: 'https://res.cloudinary.com/debewnh29/image/upload/nestly/public/pictures/cities/chennai.avif',
+      value: 'chennai',
+    },
+    {
+      title: 'Kolkata',
+      img: 'https://res.cloudinary.com/debewnh29/image/upload/nestly/public/pictures/cities/kolkata.avif',
+      value: 'kolkata',
+    },
+  ];
+
+  let cities = getCityNames();
+
+  const [searchCities, setSearchCities] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+ const handleLLocationSearch = (value) => {
+  if (!value) {
+    setSearchCities([]);
+    return;
+  }
+  const filteredCities = cities.filter((city) =>
+    city.city.toLowerCase().includes(value.toLowerCase()) ||
+    city.state.toLowerCase().includes(value.toLowerCase())
+  );
+  setSearchCities(filteredCities);
+};
+
+  console.log(searchCities);
 
   return loading ? (
     <>
@@ -229,15 +214,15 @@ export default function SearchBar() {
         <div className="hidden w-full  items-center justify-between  rounded-full border-2 border-gray-300 p-1 md:flex ">
           <div className="  flex justify-between text-sm  ">
             <div className="items-center  border-r-2 border-gray-300 px-2 py-1 ">
-              <div className="h-6 w-20  animate-pulse   rounded-l-full rounded-r-md bg-gray-200"></div>
+              <div className="h-6 w-28  animate-pulse   rounded-l-full rounded-r-md bg-gray-200"></div>
             </div>
 
             <div className="items-center  border-r-2 border-gray-300 px-2 py-1 ">
-              <div className="h-6 w-20  animate-pulse rounded-md bg-gray-200"></div>
+              <div className="h-6 w-28  animate-pulse rounded-md bg-gray-200"></div>
             </div>
 
             <div className=" items-center border-gray-300  py-1 pl-2 ">
-              <div className="h-6 w-20  animate-pulse rounded-md bg-gray-200"></div>
+              <div className="h-6 w-28  animate-pulse rounded-md bg-gray-200"></div>
             </div>
           </div>
 
@@ -265,7 +250,7 @@ export default function SearchBar() {
             <DropdownMenu>
               <DropdownMenuTrigger className="m-0 w-full    " asChild>
                 <div className="cursor-pointer   items-center rounded-full px-3 py-2 hover:bg-gray-100 focus:bg-gray-100  ">
-                  <div className="flex  h-8 w-max min-w-20 items-center justify-center text-center text-[14px]  font-semibold">
+                  <div className="flex  h-8 w-max min-w-28 items-center justify-center text-center text-[14px]  font-semibold">
                     <span className="">
                       {searchState.location
                         ? searchState.location.charAt(0).toUpperCase() +
@@ -275,13 +260,103 @@ export default function SearchBar() {
                   </div>
                 </div>
               </DropdownMenuTrigger>
-              <DropdownMenuContent align="start" className="rounded-3xl">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
+              <DropdownMenuContent
+                align="start"
+                className="w-[424px]  rounded-3xl p-5 "
+              >
+                <div className="mb-3 flex w-full items-center rounded-full border-2 focus:border-black border-gray-300 text-[13px] font-medium text-black placeholder:text-[13px] placeholder:text-black ">
+                  <input
+                    type="text"
+                    name="search-location"
+                    id="search-location"
+                    value={inputValue}
+                    onInput={(e) => {
+                      const value = e.target.value;
+                      setInputValue(value);
+                      handleLLocationSearch(value);
+                    }}
+                    placeholder="Search Destinations"
+                    className="w-full rounded-full p-2 text-black placeholder-black outline-none "
+                  />
+                  <div>
+                  {
+                    inputValue ? (
+                      <X size={32} className="cursor-pointer p-1 rounded-3xl bg-gray-200 mr-1" onClick={() => {
+                        setInputValue('');
+                        setSearchCities([]);
+                      }} />
+                    ) : null
+                  }
+               
+
+                  </div>
+                </div>
+                {inputValue ? (
+                  <ScrollArea className="mr-2 h-80 w-full select-none">
+                    <div className="flex flex-col p-1">
+                      {/* <div className='flex items-center text-sm font-semibold p-2 hover:bg-gray-200/70 rounded-xl space-x-3'>
+                        <MapPin size={32} className='p-1 rounded-3xl bg-gray-300 ' />
+                        <span>Kolkata , West Bengal</span>
+                      </div> */}
+
+                      {searchCities.length >= 1 ? (
+                        searchCities.map((city, index) => (
+                          <div
+                            key={index}
+                            onClick={() => {
+                              setSearchState((prevState) => ({
+                                ...prevState,
+                                location: city.city,
+                              }));
+                              // Force component to re-render
+                              setSearchState((prevState) => ({ ...prevState }));
+                            }}
+                            className={`flex cursor-pointer items-center space-x-3 rounded-xl p-2 text-sm font-semibold hover:bg-gray-200/70  ${searchState.location == city.city ? 'bg-gray-200' : ''} `}
+                          >
+                            <MapPin
+                              size={32}
+                              className="rounded-3xl bg-gray-100 p-1 text-gray-600 "
+                            />
+                            <span>{`${city.city} , ${city.state}`}</span>
+                          </div>
+                        ))
+                      ) : (
+                        <div className="flex items-center space-x-3 rounded-xl p-2 text-sm font-semibold ">
+                          <MapPin
+                            size={32}
+                            className="rounded-3xl bg-gray-300 p-1 "
+                          />
+                          <span>No Results Found</span>
+                        </div>
+                      )}
+                    </div>
+                  </ScrollArea>
+                ) : (
+                  <div className="grid grid-cols-3 gap-5 ">
+                    {topcities.map((city, index) => (
+                      <div
+                        key={index}
+                        onClick={() => {
+                          setSearchState((prevState) => ({
+                            ...prevState,
+                            location: city.value,
+                          }));
+                        }}
+                        className="group flex cursor-pointer flex-col space-y-1 rounded-xl p-2 hover:bg-gray-200/70"
+                      >
+                        {/* <div className="h-24 w-24 rounded-xl bg-gray-200"></div> */}
+                        <img
+                          src={city.img}
+                          alt={city.title}
+                          className="h-24 w-24 rounded-xl border-2 border-gray-300 bg-gray-100 object-cover object-center group-active:scale-95 "
+                        />
+                        <span className="text-sm font-semibold">
+                          {city.title}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                )}
               </DropdownMenuContent>
             </DropdownMenu>
 
@@ -290,7 +365,7 @@ export default function SearchBar() {
               <DropdownMenuTrigger className="m-0 w-full    " asChild>
                 <div className="cursor-pointer   items-center rounded-full px-3 py-2 hover:bg-gray-100 focus:bg-gray-100  ">
                   {' '}
-                  <div className="flex  h-8 w-max min-w-20 items-center justify-center text-center text-[14px]  font-semibold">
+                  <div className="flex  h-8 w-max min-w-28 items-center justify-center text-center text-[14px]  font-semibold">
                     <span className="">
                       {searchState.checkin && searchState.checkout
                         ? formatRangeDate(
@@ -322,7 +397,7 @@ export default function SearchBar() {
                 <DropdownMenuTrigger className="z-0 m-0 w-full   " asChild>
                   <div className=" cursor-pointer   items-center  py-2 pl-3 ">
                     <div
-                      className={`flex  h-8 w-max min-w-20 items-center justify-center text-center text-[14px] ${Number(searchState.adults) + Number(searchState.children) > 0 ? 'font-semibold' : ''}  `}
+                      className={`flex  h-8 w-max min-w-28 items-center justify-center text-center text-[14px] ${Number(searchState.adults) + Number(searchState.children) > 0 ? 'font-semibold' : ''}  `}
                     >
                       <span className="">
                         {Number(searchState.adults) +
